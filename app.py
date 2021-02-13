f1982ff0f964bd999dc939d362e5e36f3aef0bcb
darekarankita0 <55353280+darekarankita0@users.noreply.github.com>
2021-02-13 23:16:24 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS, cross_origin
+import json
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route('/', methods=['GET','POST'])
+def facts():
+    import docx2txt
+    prac = docx2txt.process("iotprac.docx")
+    #value = print(prac)
+    #data = pd.read_csv('facts.csv')
+    value = str(prac)
+    #value=(random.choices(facts))
+    #print(type(value[0]))
+    data = {
+       'Internet Of Things' : value,
+    }
+    data = json.dumps(data)
+    return jsonify(data)
+
+if __name__=='__main__':
+    #server = Server(app.wsgi_app)
+    #server.serve()
+    #server.watch('/Views/*')
+    app.run(debug=False)
+    #app.config['DEBUG'] = True
+    #app.config['TEMPLATES_AUTO_RELOAD'] = True
